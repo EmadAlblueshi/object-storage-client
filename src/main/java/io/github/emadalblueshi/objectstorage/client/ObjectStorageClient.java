eb9d570957caefbb8dee4c155772e6ddbbb513cf
Emad Alblueshi <emad.albloushi@gmail.com>
2024-06-09 19:51:41 +0300

    Remove s3 create static method

--- src/main/java/io/github/emadalblueshi/objectstorage/client/ObjectStorageClient.java
+++ src/main/java/io/github/emadalblueshi/objectstorage/client/ObjectStorageClient.java
@@ -1,19 +1,11 @@
 package io.github.emadalblueshi.objectstorage.client;
 
-import io.github.emadalblueshi.objectstorage.client.s3.S3Client;
-import io.github.emadalblueshi.objectstorage.client.s3.impl.S3ClientImpl;
 import io.vertx.codegen.annotations.VertxGen;
-import io.vertx.core.Vertx;
 
 /**
  * @author <a href="mailto:emad.albloushi@gmail.com">Emad Alblueshi</a>
  */
 @VertxGen
 public interface ObjectStorageClient {
-
-  static S3Client s3Create(Vertx vertx, ObjectStorageClientOptions objectStorageClientOptions) {
-    return new S3ClientImpl(vertx, objectStorageClientOptions);
-  }
-
   void close();
 }
